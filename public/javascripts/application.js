542f7fd35c621812bd2fb67eac96f058489f96e7
Ryan Smith <rsmith@Computer-Name.local>
2009-10-28 11:24:25 -0500

    added the mousewheel plugin

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
   var api = $("div.scrollable").scrollable({
       vertical: true,
       size: 1,
+      keyboard:false, 
       api: true
   });
+  $("div.scrollable").mousewheel();
   // scroll to today after load
   var idx = $(".today").attr("rel")
   api.seekTo(parseInt(idx-1))
